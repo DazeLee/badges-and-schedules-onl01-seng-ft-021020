61f7879c65a964185a789ca71eb95df768d5fed4
Daisy Olmo-Rivera <dazelee@yahoo.com>
2020-02-20 01:03:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,12 +2,12 @@ def batch_badge_creator
   
   ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus","Matz"]
   
-  puts "hello my name is"
+  puts "hello my name is #{name}"
   
 end
   
   def assign_rooms
   
-  puts "Hello, []! You'll be assignrd to room[]!"
+  puts "Hello, #{name}! You'll be assignrd to room #{num}!"
   
   # Write your code here.
